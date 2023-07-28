ce8d1e07452ab3955bec812d084dea9cd4a669a0
noahwu8888 <noahwu8888@gmail.com>
2023-07-28 12:00:48 +0800

    Imported Pistol Enemy

---
+++ Assets/Scripts/ProjecticleScript.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjecticleScript : MonoBehaviour
+{
+    public float speed;
+    public float damage;
+    public float timeOfBullet;
+
+    private Transform player;
+    private Transform selfTransform;
+
+    private Vector2 target;
+    private Vector3 direction;
+    private Vector3 playerPosition;
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+
+        target = new Vector2(player.position.x, player.position.y);
+
+
+        selfTransform = GetComponent<Transform>();
+        playerPosition = player.position;
+        direction = (playerPosition - selfTransform.position).normalized;
+    }
+    void Update()
+    {
+        selfTransform.position += direction * speed * Time.deltaTime;
+
+        
+        StartCoroutine(bulletTime(timeOfBullet));
+        
+    }
+    private IEnumerator bulletTime(float time)
+    {
+        yield return new WaitForSeconds(time);
+        DestroyProjectile();
+        
+    }
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            DestroyProjectile();
+            other.GetComponent<PlayerHealth>().TakeDamage(damage);
+
+        }
+    }
+
+    void DestroyProjectile()
+    {
+        Destroy(gameObject);
+    }
+
+}
---
+++ Assets/Scripts/ShooterEnemy.cs
@@ -0,0 +1,68 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShooterEnemy : MonoBehaviour
+{
+    public float speed;
+    public float reloadSpeed;
+    public float timeBetweenShots;
+    public float stoppingDistance;
+    public float nearDistance;
+    public float totalNumberOfShots;
+    private float numberOfShots;
+
+
+    public GameObject shot;
+    private Transform player;
+
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+        numberOfShots = totalNumberOfShots;
+
+        SpawnBullet();
+    }
+    void Update()
+    {
+        Movement();
+
+    }
+    private void Movement()
+    { if (Vector2.Distance(transform.position, player.position) < nearDistance) 
+        { 
+        transform.position = Vector2.MoveTowards(transform.position, player.position, -speed * Time.deltaTime);
+        }   
+     else if (Vector2.Distance(transform.position, player.position) > stoppingDistance)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, player.position, speed * Time.deltaTime);
+        }
+     else if (Vector2.Distance(transform.position, player.position) < stoppingDistance && Vector2.Distance(transform.position, player.position) > nearDistance)
+        {
+            transform.position = this.transform.position;
+        }}
+    private void SpawnBullet()
+    {
+    if(numberOfShots > 0) 
+        {
+            numberOfShots -= 1;
+            Instantiate(shot, transform.position, Quaternion.identity);
+            StartCoroutine(shotDelay(timeBetweenShots));
+        }
+    else
+        {
+            StartCoroutine(reload(reloadSpeed));
+        }
+    }
+    private IEnumerator reload(float time)
+    {   
+        yield return new WaitForSeconds(time); 
+        numberOfShots = totalNumberOfShots;
+        SpawnBullet();
+    }
+    private IEnumerator shotDelay(float time)
+    {   
+        yield return new WaitForSeconds(time);
+        SpawnBullet();
+    }
+}
