29ccba269f1096fabf47edad28339efea078adae
noahwu8888 <noahwu8888@gmail.com>
2023-08-02 15:59:12 +0800

    Reward System Implemented, Reward not given

---
+++ Assets/Scripts/RewardManager.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+
+public class RewardManager : MonoBehaviour
+{
+    public GameObject[] Rewards;
+    private GameObject[] enemies;
+    private int rewardNum;
+    public int maxRewardNum;
+    private bool gaveRewardFlag;
+
+    private void OnEnable()
+    {
+        ResetManager.OnReset += HandleReset;
+    }
+
+    private void OnDisable()
+    {
+        ResetManager.OnReset -= HandleReset;
+    }
+
+    private void HandleReset(int resetCount)
+    {
+        enemies = GameObject.FindGameObjectsWithTag("Enemy");
+        rewardNum = Mathf.Min(resetCount,maxRewardNum);
+        gaveRewardFlag = false;
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        enemies = GameObject.FindGameObjectsWithTag("Enemy");
+        if (other.CompareTag("Player"))
+        {
+            if (enemies.Length == 0 && !gaveRewardFlag)
+            {
+                Instantiate(Rewards[rewardNum], new Vector3(0, 0, 0), Quaternion.identity);
+                gaveRewardFlag = true;
+            }
+        }
+    }
+}
